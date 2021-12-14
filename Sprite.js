ea5b94c406670642688e7a7836271ad95efdc315
gabrieldiass <gabriel_eduardo_dias@hotmail.com>
2021-12-13 23:26:48 -0300

    Inserindo o Jogo

---
+++ Sprite.js
@@ -0,0 +1,40 @@
+function Sprite(x, y, largura, altura) {
+    this.x = x;
+    this.y = y;
+    this.largura = largura;
+    this.altura = altura;
+
+    this.desenha = function (xCanvas, yCanvas) {
+        ctx.drawImage(
+            img, //Imagem a ser carregada
+            this.x, //Desenhar a partir de x
+            this.y, //Desenhar a partir de y
+            this.largura, //Desenhar até Largura total
+            this.altura, //Desenhar até Altura total
+            xCanvas, //X que queremos desenhar na Canvas
+            yCanvas, //Y que queremos desenhar na Canvas
+            this.largura, //Escala X
+            this.altura //Escala Y
+        );
+    }
+}
+
+var bg = new Sprite(0, 0, 800, 600), 
+spriteBoneco = new Sprite(980, 7, 104, 104),
+spriteCheirado = new Sprite(1215, 23, 126, 126),
+spriteBonecoDano = new Sprite(845, 8, 104, 104),
+spritePoder1 = new Sprite(12, 1056, 130, 109),
+spritePoder2 = new Sprite(169, 1049, 140, 124),
+perdeu = new Sprite(944, 432, 266, 324),
+jogar = new Sprite(897, 128, 292, 250),
+novo = new Sprite(76, 761, 254, 115),
+spriteRecorde = new Sprite(79, 892, 310, 92),
+spriteChao = new Sprite(0, 607, 800, 85),
+obstaculoAzul = new Sprite(578, 967, 50, 220);
+obstaculoVerde = new Sprite(642, 967, 50, 220);
+obstaculoVermelho = new Sprite(768, 967, 50, 220);
+obstaculoAmarelo = new Sprite(705, 967, 50, 220);
+obstaculoRoxo = new Sprite(832, 967, 50, 220);
+obstaculoMarrom = new Sprite(895, 967, 50, 220);
+obstaculoCinza = new Sprite(958, 967, 50, 220);
+voador1 = new Sprite(12, 1127, 191, 60);
